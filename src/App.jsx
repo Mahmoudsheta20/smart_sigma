3844d7843630424df4a8a1777be3abeb10e38327
mahmoudsheta20 <shetamahmoud463@gmail.com>
2023-11-01 10:23:03 +0200

    11/1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Login from "./pages/Login";
+function App() {
+  return (
+    <div className="App bg-[#DCDCDC] min-h-screen w-[100%]">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
